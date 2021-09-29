435afb60274aae87ab19a54f345542c33dd4b13e
ahmed-saleh98 <ahmed.mohamed.saleh1998@gmail.com>
2021-09-29 19:49:52 +0200

    Updates

---
+++ precache-manifest.dfe83815cf8eeaf75a4ee7b753bcca23.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca9bb07556b38bda6e7a",
+    "url": "/MyReads-react-project/static/css/main.ba883617.chunk.css"
+  },
+  {
+    "revision": "ca9bb07556b38bda6e7a",
+    "url": "/MyReads-react-project/static/js/main.ca9bb075.chunk.js"
+  },
+  {
+    "revision": "25b7c81548280dacde8e",
+    "url": "/MyReads-react-project/static/js/1.25b7c815.chunk.js"
+  },
+  {
+    "revision": "a1936a30880f85ca67d4",
+    "url": "/MyReads-react-project/static/js/runtime~main.a1936a30.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/MyReads-react-project/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/MyReads-react-project/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/MyReads-react-project/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "17a9ed7750a388bbd80722e2ce0a7a8d",
+    "url": "/MyReads-react-project/index.html"
+  }
+];
